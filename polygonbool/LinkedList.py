28e1d686f4f1807263e4c3a498c47c282e16be57
Pranav <thepranavtotla@gmail.com>
2019-09-11 15:35:26 +0530

    Added init method to SinglyLinkedList

--- polygonbool/LinkedList.py
+++ polygonbool/LinkedList.py
@@ -10,3 +10,16 @@ class Node:
         if self.data == other.data:
             return True
         return False
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        nodes = []
+        curr = self.head
+        while curr:
+            nodes.append(repr(curr))
+            curr = curr.next
+        return '[' + ','.join(nodes) + ']'
