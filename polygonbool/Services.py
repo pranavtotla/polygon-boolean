43e92ce419ef8cc31b2c7aad022aff5696b72185
Pranav <thepranavtotla@gmail.com>
2019-10-01 15:01:24 +0530

    Added orientation in Services

---
+++ polygonbool/Services.py
@@ -0,0 +1,5 @@
+def orientation(p, q, r):
+    val = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
+    if val == 0: return 0  # Collinear
+    if val > 0: return 1  # Clockwise
+    return 2  # Anti clockwise
