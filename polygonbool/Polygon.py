a7282ab808f2d87d8715be8d9f6ecc5c5f1b61a2
Pranav <thepranavtotla@gmail.com>
2019-10-01 16:20:11 +0530

    Added contains method to Polygon class

--- polygonbool/Polygon.py
+++ polygonbool/Polygon.py
@@ -1,4 +1,4 @@
-from .Services import do_intersect
+from .Services import do_intersect, orientation, on_segment
 
 class Polygon:
     points = None
@@ -28,3 +28,24 @@ class Polygon:
         :return: float
         """
         return max(point[0] for point in self.points)
+
+    def contains(self, point):
+        """
+        Checks if the point lies inside the polygon.
+        :param point: Point
+        :return: bool
+        """
+        infinity = [self.max_x() + 1, point[1]]
+        count = 0
+
+        for i in range(len(self.points)):
+            edge = [self.points[i], self.points[(i + 1) % len(self.points)]]
+            ray = [point, infinity]
+
+            if do_intersect(edge, ray):
+                if orientation(edge[0], point, edge[1]) == 0:  # Points are collinear
+                    return on_segment(edge[0], point, edge[1])
+                count += 1
+
+        return count % 2 == 1
+    
