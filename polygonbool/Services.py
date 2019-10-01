32b72e3d54592100293041ae1ffef8a8607406ef
Pranav <thepranavtotla@gmail.com>
2019-10-01 15:37:29 +0530

    Added function on_segment to Services

--- polygonbool/Services.py
+++ polygonbool/Services.py
@@ -3,3 +3,10 @@ def orientation(p, q, r):
     if val == 0: return 0  # Collinear
     if val > 0: return 1  # Clockwise
     return 2  # Anti clockwise
+
+# Checks if q lies on the segment pr
+def on_segment(p, q, r):
+    if q[0] <= max(p[0], r[0]) and q[0] >= min(p[0], r[0]) and q[1] <= max(p[1], r[1]) and q[1] >= min(p[1], r[1]):
+        return True 
+    return False
+
